fd34597adeaec227081e951d730c59854df2ddf1
Zizheng Wu <me@zizhengwu.com>
2015-11-21 18:12:21 -0500

    merge two to get third

--- Agent.py
+++ Agent.py
@@ -29,11 +29,11 @@ class Agent:
         pass
 
     def calculate_figure_pixel_similarity(self, pixel_matrix_a, pixel_matrix_b):
-        intersection = functools.reduce(lambda x, y: x+y, self.figure_pixel_matrix_intersection(pixel_matrix_a, pixel_matrix_b)).count(255)
-        union = functools.reduce(lambda x, y: x+y, self.figure_pixel_matrix_union(pixel_matrix_a, pixel_matrix_b)).count(255)
+        intersection = functools.reduce(lambda x, y: x+y, self.figure_pixel_matrix_white_intersection(pixel_matrix_a, pixel_matrix_b)).count(255)
+        union = functools.reduce(lambda x, y: x+y, self.figure_pixel_matrix_white_union(pixel_matrix_a, pixel_matrix_b)).count(255)
         return intersection / union * 100
 
-    def figure_pixel_matrix_union(self, pixel_matrix_a, pixel_matrix_b):
+    def figure_pixel_matrix_white_union(self, pixel_matrix_a, pixel_matrix_b):
         return_matrix = [[0 for i in range(len(pixel_matrix_a[0]))] for j in range(len(pixel_matrix_a))]
         for row, (a_row, b_row) in enumerate(zip(pixel_matrix_a, pixel_matrix_b)):
             for column, (a_row_column, b_row_column) in enumerate(zip(a_row, b_row)):
@@ -43,7 +43,27 @@ class Agent:
                     return_matrix[row][column] = 0
         return return_matrix
 
-    def figure_pixel_matrix_intersection(self, pixel_matrix_a, pixel_matrix_b):
+    def figure_pixel_matrix_black_union(self, pixel_matrix_a, pixel_matrix_b):
+        return_matrix = [[0 for i in range(len(pixel_matrix_a[0]))] for j in range(len(pixel_matrix_a))]
+        for row, (a_row, b_row) in enumerate(zip(pixel_matrix_a, pixel_matrix_b)):
+            for column, (a_row_column, b_row_column) in enumerate(zip(a_row, b_row)):
+                if a_row_column == 0 or b_row_column == 0:
+                    return_matrix[row][column] = 0
+                else:
+                    return_matrix[row][column] = 255
+        return return_matrix
+
+    def figure_pixel_matrix_black_intersection(self, pixel_matrix_a, pixel_matrix_b):
+        return_matrix = [[0 for i in range(len(pixel_matrix_a[0]))] for j in range(len(pixel_matrix_a))]
+        for row, (a_row, b_row) in enumerate(zip(pixel_matrix_a, pixel_matrix_b)):
+            for column, (a_row_column, b_row_column) in enumerate(zip(a_row, b_row)):
+                if a_row_column == 0 and b_row_column == 0:
+                    return_matrix[row][column] = 0
+                else:
+                    return_matrix[row][column] = 255
+        return return_matrix
+
+    def figure_pixel_matrix_white_intersection(self, pixel_matrix_a, pixel_matrix_b):
         return_matrix = [[0 for i in range(len(pixel_matrix_a[0]))] for j in range(len(pixel_matrix_a))]
         for row, (a_row, b_row) in enumerate(zip(pixel_matrix_a, pixel_matrix_b)):
             for column, (a_row_column, b_row_column) in enumerate(zip(a_row, b_row)):
@@ -77,12 +97,23 @@ class Agent:
         best_answer = -1
         similarity_maximum = 0
         if self.calculate_figure_pixel_similarity(
-                self.figure_pixel_matrix_intersection(self.figure_pixel_matrix_intersection(self.figure_pixel_matrix['A'], self.figure_pixel_matrix['B']), self.figure_pixel_matrix['C']),
-                self.figure_pixel_matrix_intersection(self.figure_pixel_matrix_intersection(self.figure_pixel_matrix['D'], self.figure_pixel_matrix['E']), self.figure_pixel_matrix['F'])) > 95:
+                self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix['A'], self.figure_pixel_matrix['B']), self.figure_pixel_matrix['C']),
+                self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix['D'], self.figure_pixel_matrix['E']), self.figure_pixel_matrix['F'])) > 95:
             for possible_answer in ['1', '2', '3', '4', '5', '6', '7', '8']:
                 this_similarity = self.calculate_figure_pixel_similarity(
-                        self.figure_pixel_matrix_intersection(self.figure_pixel_matrix_intersection(self.figure_pixel_matrix['A'], self.figure_pixel_matrix['B']), self.figure_pixel_matrix['C']),
-                        self.figure_pixel_matrix_intersection(self.figure_pixel_matrix_intersection(self.figure_pixel_matrix['G'], self.figure_pixel_matrix['H']), self.figure_pixel_matrix[possible_answer]))
+                        self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix['A'], self.figure_pixel_matrix['B']), self.figure_pixel_matrix['C']),
+                        self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix_white_intersection(self.figure_pixel_matrix['G'], self.figure_pixel_matrix['H']), self.figure_pixel_matrix[possible_answer]))
+                if this_similarity > similarity_maximum:
+                    best_answer = possible_answer
+                    similarity_maximum = this_similarity
+        return best_answer
+
+    def method_merge_two_to_get_third(self):
+        best_answer = -1
+        similarity_maximum = 0
+        if self.calculate_figure_pixel_similarity(self.figure_pixel_matrix_black_union(self.figure_pixel_matrix['A'], self.figure_pixel_matrix['B']), self.figure_pixel_matrix['C']) > 95:
+            for possible_answer in ['1', '2', '3', '4', '5', '6', '7', '8']:
+                this_similarity = self.calculate_figure_pixel_similarity(self.figure_pixel_matrix_black_union(self.figure_pixel_matrix['G'], self.figure_pixel_matrix['H']), self.figure_pixel_matrix[possible_answer])
                 if this_similarity > similarity_maximum:
                     best_answer = possible_answer
                     similarity_maximum = this_similarity
@@ -91,7 +122,7 @@ class Agent:
     # noinspection PyPep8Naming
     def Solve(self, problem: RavensProblem):
         self.initialize(problem)
-        for possible_method in [self.method_unchanged, self.method_simple_iterate]:
+        for possible_method in [self.method_unchanged, self.method_merge_two_to_get_third, self.method_simple_iterate]:
             possible_answer = possible_method()
             if possible_answer != -1:
                 print(possible_method.__name__)
