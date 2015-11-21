086bd3c7b112a335186cd32ff7abfde7b2835dc4
Zizheng Wu <me@zizhengwu.com>
2015-11-21 02:34:02 -0500

    traverse problems, open images, transform images into pixels

--- Agent.py
+++ Agent.py
@@ -8,11 +8,22 @@ import pprint
 import os
 
 
+# noinspection PyMethodMayBeStatic
 class Agent:
     def __init__(self):
         self.log_file = None
 
+    def initialize(self, problem: RavensProblem):
+        print(problem.name)
+        folder_name = 'Problems/{}s {}/{}/'.format(problem.name.rsplit(' ', 1)[0], problem.name.split('-')[0].rsplit(' ', 1)[1], problem.name)
+        image = Image.open('{}{}.png'.format(folder_name, problem.name)).convert('1')
+        pixels = list(image.getdata())
+        width, height = image.size
+        pixels = [pixels[i * width:(i + 1) * width] for i in range(height)]
+        pass
+
     # noinspection PyPep8Naming
     def Solve(self, problem: RavensProblem):
-        print(problem.name)
+        self.initialize(problem)
         return -1
+
